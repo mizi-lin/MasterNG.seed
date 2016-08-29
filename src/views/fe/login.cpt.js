37ddd05569b142dbcbf4a9ddd2400df0494b175e
amily4555 <amily4555@gmail.com>
2016-08-29 11:33:54 +0800

    fixed login

--- src/views/fe/login.cpt.js
+++ src/views/fe/login.cpt.js
@@ -21,6 +21,10 @@ var LoginCpt = (function () {
         this.$$ = $$;
         this.G = G;
         this.router = router;
+        this.fm = {
+            email: '',
+            password: ''
+        };
         this.fm = this.G.ENV_CONST.TEST_ACCOUNT;
     }
     LoginCpt.prototype.save = function (form) {
