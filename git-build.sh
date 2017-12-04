9deaea625529e3e9af7b960a13b82a8502990e67
amily4555 <amily4555@gmail.com>
2017-12-04 10:58:35 +0800

    fixed or changes

--- git-build.sh
+++ git-build.sh
@@ -12,7 +12,7 @@ fi
 
 git pull
 
-node --max_old_space_size=8192 node_modules/@angular/cli/bin/ng build --prod --build-optimizer=false
+node --max_old_space_size=8192 node_modules/@angular/cli/bin/ng build --prod --build-optimizer=true
 
 git add .
 
